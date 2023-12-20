fcca546af8618731f283da6d124d71dbddded0ab
Ashish Utagikar <aputagikar@gmail.com>
2023-12-20 09:36:13 -0500

    First versions

---
+++ src/test/java/com/avinash/madha/SpringBootMavenPluginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avinash.madha;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMavenPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
