54a99c8d5372c655f073b6a51911e46a63cd589a
autagika <aputagikar@gmail.com>
2025-01-19 18:08:11 -0500

    Update lmn.java

--- src/main/java/com/avinash/madha/service/lmn.java
+++ src/main/java/com/avinash/madha/service/lmn.java
@@ -3,5 +3,9 @@ package com.avinash.madha.service;
 public class lmn {
     public static void main(String[] args) {
         System.out.println("My name is Ashish");
+        System.out.println("I went to Gurukul High School in Ghatkopar,Mumbai");
+        System.out.println("I went to Ruia College in Matunga,Mumbai");
+        System.out.println("I went to Banaras Hindu University, Varanasi");
+        System.out.println("I went to Arizona State University, Arizona");
     }
 }
