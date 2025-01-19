e4e760639cebffb79a149ecbd35a2d20861194eb
Ashish Utagikar <aputagikar@gmail.com>
2025-01-19 18:59:23 -0500

    Additional Information about myself in three files on the featureA branch

---
+++ src/main/java/com/avinash/madha/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.avinash.madha.controllers;
+
+import com.avinash.madha.dao.HomeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    HomeDao dao;
+
+    @GetMapping("/info")
+    public String getInformation() {
+        return dao.fetchInformation();
+    }
+}
