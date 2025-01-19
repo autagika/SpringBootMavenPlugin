1f42a531d4a82fc18ed4bda952151901c3d29658
autagika <aputagikar@gmail.com>
2025-01-19 18:04:18 -0500

    Update abc.java

--- src/main/java/com/avinash/madha/controllers/abc.java
+++ src/main/java/com/avinash/madha/controllers/abc.java
@@ -3,5 +3,7 @@ package com.avinash.madha.controllers;
 public class abc {
     public static void main(String[] args) {
         System.out.println("My name is Ashish");
+        System.out.println("Initially I used to live in Phoenix, Arizona");
+        System.out.println("I lived in Arizona for 20 years");
     }
 }
