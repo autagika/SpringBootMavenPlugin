d2675211b6cfd6853801d038546a3be69a4550e7
Ashish Utagikar <aputagikar@gmail.com>
2025-01-19 22:08:41 -0500

    Revert "Additional Information about myself in three files on the featureA branch"
    This reverts commit d356ef92

--- src/main/java/com/avinash/madha/service/lmn.java
+++ src/main/java/com/avinash/madha/service/lmn.java
@@ -2,6 +2,12 @@ package com.avinash.madha.service;
 
 public class lmn {
     public static void main(String[] args) {
-        System.out.println("My name is Ashish");
+        System.out.println("My name is Ashish Utagikar");
+        System.out.println("I went to Gurukul High School in Ghatkopar,Mumbai, India");
+        System.out.println("I went to Ruia College in Matunga,Mumbai, India");
+        System.out.println("I went to Banaras Hindu University, Lanka, Varanasi, India");
+        System.out.println("I went to Arizona State University, Phoenix, Arizona,USA");
+        System.out.println("I went to Arizona State University, Phoenix, Arizona,USA");
+        System.out.println("I went to Arizona State University, Phoenix, Arizona,USA");
     }
 }
