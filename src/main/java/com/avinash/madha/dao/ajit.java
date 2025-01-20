a4b921924eb5ac580d8706cd28b85b5085596170
Ashish Utagikar <aputagikar@gmail.com>
2025-01-19 19:32:28 -0500

    Additional Information about myself in three files on the featureA branch

---
+++ src/main/java/com/avinash/madha/dao/ajit.java
@@ -0,0 +1,8 @@
+package com.avinash.madha.dao;
+
+public class ajit {
+    public static void main(String[] args) {
+        System.out.println("My name is Ashish");
+        System.out.println("I am the son of Purushottam Ganesh Utagikar");
+    }
+}
---
+++ src/main/java/com/avinash/madha/service/kunal.java
@@ -0,0 +1,8 @@
+package com.avinash.madha.service;
+
+public class kunal {
+    public static void main(String[] args) {
+        System.out.println("My name is Ashish");
+        System.out.println("I am the son of Purushottam Ganesh Utagikar");
+    }
+}
