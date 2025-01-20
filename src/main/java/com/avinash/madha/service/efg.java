e11903a2feb45ed348fe07182bea49648f6012c7
Ashish Utagikar <aputagikar@gmail.com>
2025-01-19 21:15:32 -0500

    Additional Information about myself in three files on the featureA branch

--- src/main/java/com/avinash/madha/service/efg.java
+++ src/main/java/com/avinash/madha/service/efg.java
@@ -3,6 +3,5 @@ package com.avinash.madha.service;
 public class efg {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("I like Astronomy");
     }
 }
