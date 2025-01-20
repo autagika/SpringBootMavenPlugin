83e0519f45b3073c582aacdeb1ad3a261a366cd7
Ashish Utagikar <aputagikar@gmail.com>
2025-01-19 19:23:40 -0500

    Additional Information about myself in three files on the featureA branch

---
+++ src/main/java/com/avinash/madha/controllers/SchoolController.java
@@ -0,0 +1,12 @@
+package com.avinash.madha.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SchoolController {
+    @GetMapping("/name")
+    public String getSchoolName() {
+        return "Gurukul";
+    }
+}
